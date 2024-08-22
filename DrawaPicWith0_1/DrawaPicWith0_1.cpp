474973171fc9944b62f08e694bbfe22acc33cb5a
MkaGodh <fenryrvla@yandex.ru>
2024-08-22 18:03:54 +0300

    Добавьте файлы проекта.

---
+++ DrawaPicWith0_1/DrawaPicWith0_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+
+int main()
+{
+    int width, length;
+    std::cout << "Please input weight: " << std::endl;
+    std::cin >> width;
+    std::cout << "Please input length: " << std::endl;
+    std::cin >> length;
+
+    std::ofstream pic("pic.txt");
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < width; ++j) {
+            pic << std::rand() % 2;
+        }
+        pic << "\n";
+    }
+    pic.close();
+
+    return  0;
+}
+
